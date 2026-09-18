5fc994a229d9f8992f59e1b2f876d77acb4d1395
Adam D. Cornett <adc@redhat.com>
2026-09-18 14:04:56 -0700

    creating centeralized default for default flag values
    Signed-off-by: Adam D. Cornett <adc@redhat.com>

---
+++ cmd/defaults.go
@@ -0,0 +1,10 @@
+package cmd
+
+const (
+	artifactsPath   = "artifacts"
+	artifactsTarget = "."
+	ciEnvironment   = "common"
+	docsType        = "markdown"
+	jobOutputPath   = "prowjob-base-url"
+	pfltLogLevel    = "trace"
+)
